e7d9ecbcc55fff3371c84ba501515d828498f66e
Rob <87914992+robbiefish@users.noreply.github.com>
2022-02-10 13:33:20 -0500

    Checks if there is a * character in the string before parsing fully (#6)

--- src/ntrip_client/nmea_parser.py
+++ src/ntrip_client/nmea_parser.py
@@ -1,6 +1,8 @@
 import logging
 
 _NMEA_MAX_LENGTH = 82
+_NMEA_MIN_LENGTH = 3
+_NMEA_CHECKSUM_SEPERATOR = "*"
 
 class NMEAParser:
 
@@ -17,6 +19,10 @@ class NMEAParser:
       self._logwarn('Received invalid NMEA sentence. Max length is {}, but sentence was {} bytes'.format(_NMEA_MAX_LENGTH, len(sentence)))
       self._logwarn('Sentence: {}'.format(sentence))
       return False
+    if len(sentence) < _NMEA_MIN_LENGTH:
+      self._logwarn('Received invalid NMEA sentence. We need at least {} bytes to parse but got {} bytes'.format(_NMEA_MIN_LENGTH, len(sentence)))
+      self._logwarn('Sentence: {}'.format(sentence))
+      return False
     if sentence[0] != '$' and sentence[0] != '!':
       self._logwarn('Received invalid NMEA sentence. Sentence should begin with "$" or "!", but instead begins with {}'.format(sentence[0]))
       self._logwarn('Sentence: {}'.format(sentence))
@@ -25,9 +31,13 @@ class NMEAParser:
       self._logwarn('Received invalid NMEA sentence. Sentence should end with \\r\\n, but instead ends with {}'.format(sentence[-2:]))
       self._logwarn('Sentence: {}'.format(sentence))
       return False
+    if _NMEA_CHECKSUM_SEPERATOR not in sentence:
+      self._logwarn('Received invalid NMEA sentence. Sentence should have a "{}" character to seperate the checksum, but we could not find it.'.format(_NMEA_CHECKSUM_SEPERATOR))
+      self._logwarn('Sentence: {}'.format(sentence))
+      return False
 
     # Checksum check
-    data, expected_checksum_str = sentence.rsplit('*', 1)
+    data, expected_checksum_str = sentence.rsplit(_NMEA_CHECKSUM_SEPERATOR, 1)
     expected_checksum = int(expected_checksum_str, 16)
     calculated_checksum = 0
     for char in data[1:]:
