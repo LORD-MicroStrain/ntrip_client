4952eecb75d6836b8f07d210da8b039716972a82
Rob Fisher <robbie.fish76@gmail.com>
2022-07-21 18:54:12 +0000

    Adds ability to publish an rtcm_msgs Message instead of a mavros_msgs RTCM

--- scripts/ntrip_ros.py
+++ scripts/ntrip_ros.py
@@ -3,14 +3,25 @@
 import os
 import sys
 import json
+import importlib
 
 import rospy
 from std_msgs.msg import Header
-from mavros_msgs.msg import RTCM
 from nmea_msgs.msg import Sentence
 
 from ntrip_client.ntrip_client import NTRIPClient
 
+# Try to import a couple different types of RTCM messages
+_MAVROS_MSGS_NAME = "mavros_msgs"
+_RTCM_MSGS_NAME = "rtcm_msgs"
+have_mavros_msgs = False
+have_rtcm_msgs = False
+if importlib.util.find_spec(_MAVROS_MSGS_NAME) is not None:
+  have_mavros_msgs = True
+  from mavros_msgs.msg import RTCM as mavros_msgs_RTCM
+if importlib.util.find_spec(_RTCM_MSGS_NAME) is not None:
+  have_rtcm_msgs = True
+  from rtcm_msgs.msg import Message as rtcm_msgs_RTCM
 
 class NTRIPRos:
   def __init__(self):
@@ -52,9 +63,26 @@ class NTRIPRos:
     # Read an optional Frame ID from the config
     self._rtcm_frame_id = rospy.get_param('~rtcm_frame_id', 'odom')
 
+    # Determine the type of RTCM message that will be published
+    rtcm_msgs_pkg = rospy.get_param('~rtcm_msg_pkg', _MAVROS_MSGS_NAME)
+    if rtcm_msgs_pkg == _MAVROS_MSGS_NAME:
+      if have_mavros_msgs:
+        self._rtcm_msg_type = mavros_msgs_RTCM
+        self._create_rtcm_message = self._create_mavros_msgs_rtcm_message
+      else:
+        rospy.logfatal('The requested RTCM package {} is a valid option, but we were unable to import it. Please make sure you have it installed'.format(rtcm_msgs_pkg))
+    elif rtcm_msgs_pkg == _RTCM_MSGS_NAME:
+      if have_rtcm_msgs:
+        self._rtcm_msg_type = rtcm_msgs_RTCM
+        self._create_rtcm_message = self._create_rtcm_msgs_rtcm_message
+      else:
+        rospy.logfatal('The requested RTCM package {} is a valid option, but we were unable to import it. Please make sure you have it installed'.format(rtcm_msgs_pkg))
+    else:
+      rospy.logfatal('The RTCM package {} is not a valid option. Please choose between the following packages {}'.format(rtcm_msgs_pkg, str.join([_MAVROS_MSGS_NAME, _RTCM_MSGS_NAME])))
+
     # Setup the RTCM publisher
     self._rtcm_timer = None
-    self._rtcm_pub = rospy.Publisher('rtcm', RTCM, queue_size=10)
+    self._rtcm_pub = rospy.Publisher('rtcm', self._rtcm_msg_type, queue_size=10)
 
     # Initialize the client
     self._client = NTRIPClient(
@@ -103,13 +131,25 @@ class NTRIPRos:
 
   def publish_rtcm(self, event):
     for raw_rtcm in self._client.recv_rtcm():
-      self._rtcm_pub.publish(RTCM(
-        header=Header(
-          stamp=rospy.Time.now(),
-          frame_id=self._rtcm_frame_id
-        ),
-        data=raw_rtcm
-      ))
+      self._rtcm_pub.publish(self._create_rtcm_message(raw_rtcm))
+
+  def _create_mavros_msgs_rtcm_message(self, rtcm):
+    return mavros_msgs_RTCM(
+      header=Header(
+        stamp=rospy.Time.now(),
+        frame_id=self._rtcm_frame_id
+      ),
+      data=rtcm
+    )
+
+  def _create_rtcm_msgs_rtcm_message(self, rtcm):
+    return rtcm_msgs_RTCM(
+      header=Header(
+        stamp=rospy.Time.now(),
+        frame_id=self._rtcm_frame_id
+      ),
+      message=rtcm
+    )
 
 
 if __name__ == '__main__':
